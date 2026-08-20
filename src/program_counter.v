e052ccda9aee34d76eaa1fd95c90e5684fe2e06e
Muhammad Sudais <mhmmad.sudais@gmail.com>
2026-08-21 03:11:30 +0500

    Update program_counter.v

--- src/program_counter.v
+++ src/program_counter.v
@@ -1,30 +1,8 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/04/2026 10:22:16 PM
-// Design Name: 
-// Module Name: program_counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module program_counter(clk, rst, pc);
-
     input clk, rst;
     output reg [31:0] pc;
-
     always @(posedge clk)
     begin
         if (rst) 
@@ -32,5 +10,4 @@ module program_counter(clk, rst, pc);
         else 
             pc <= pc + 4;
     end
-
 endmodule
